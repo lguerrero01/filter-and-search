7d755054723c0cf201523f51fbc1d022170df348
lguerrero1 <luisguerrero12387@gmail.com>
2021-03-09 13:35:38 -0400

    filter

--- js/app.js
+++ js/app.js
@@ -1,10 +1,76 @@
+//selectores 
+const marca = document.querySelector('#marca');
+const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+
+
 // variables 
 let resultados = document.querySelector('#resultado');
-const year = document.querySelector('#year');
 
 const max = new Date().getFullYear();
 const min = max - 10;
  console.log( max)
+
+//generar un objeto
+const datosBusqueda = {
+    marca : '',
+    year: '',
+    minimo : '',
+    maximo: '',
+    puertas: '',
+    transmision:'',
+    color:''
+}
+
+marca.addEventListener('change', e => {
+    datosBusqueda.marca = e.target.value;
+
+    // Mandar llamar la función de filtrar Autos
+    filtrarAuto();
+});
+
+year.addEventListener('input', e => {
+    datosBusqueda.year = Number(e.target.value);
+    // Mandar llamar la función de filtrar Autos
+    filtrarAuto();
+});
+
+minimo.addEventListener('input', e => {
+    datosBusqueda.minimo = Number(e.target.value);
+    // Mandar llamar la función de filtrar Autos
+    filtrarAuto();
+});
+
+maximo.addEventListener('input', e => {
+    datosBusqueda.maximo = Number(e.target.value);
+    // Mandar llamar la función de filtrar Autos
+    filtrarAuto();
+});
+
+puertas.addEventListener('input', e => {
+    datosBusqueda.puertas = Number(e.target.value);
+    // Mandar llamar la función de filtrar Autos
+    filtrarAuto();
+});
+
+transmision.addEventListener('input', e => {
+    datosBusqueda.transmision = e.target.value
+    // Mandar llamar la función de filtrar Autos
+    filtrarAuto();
+});
+
+color.addEventListener('input', e => {
+    datosBusqueda.color = e.target.value
+    // Mandar llamar la función de filtrar Autos
+    filtrarAuto();
+});
+console.log(datosBusqueda);
+
 //eventos 
 document.addEventListener('DOMContentLoaded', () => {
     mostrarAutos();// muestra los automoviles al cargar
@@ -39,4 +105,62 @@ function llenarSelect () {
         opcion.textContent = i; 
         year.appendChild(opcion);
     }
+}
+
+function filtrarAuto () {
+    const resultado = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo).filter(filtrarPuertas).filter(filtrarTransmision).filter(filtrarColor);
+
+    //    console.log(resultado);
+       if(resultado.length){
+            mostrarAutos(resultado);
+       } else {
+           noResultado();
+       }
+}
+
+// Aplica los filtros
+function filtrarMarca(auto) {
+    if(datosBusqueda.marca){
+        return auto.marca === datosBusqueda.marca;
+    } 
+    return auto;
+}
+function filtrarYear(auto) {
+    if(datosBusqueda.year){
+        return auto.year === datosBusqueda.year;
+    }
+    return auto;
+}
+
+function filtrarMinimo(auto) {
+    if(datosBusqueda.minimo){
+        return auto.precio >= datosBusqueda.minimo;
+    }
+    return auto;
+}
+function filtrarMaximo(auto) {
+    if(datosBusqueda.maximo){
+        return auto.precio <= datosBusqueda.maximo;
+    }
+    return auto;
+}
+function filtrarPuertas(auto) {
+    if(datosBusqueda.puertas){
+        return auto.puertas === datosBusqueda.puertas;
+    }
+    return auto;
+}
+
+function filtrarTransmision(auto) {
+    if(datosBusqueda.transmision){
+        return auto.transmision === datosBusqueda.transmision;
+    } 
+    return auto;
+}
+
+function filtrarColor(auto){
+    if(datosBusqueda.color){
+        return auto.color === datosBusqueda.color;
+    } 
+    return  auto;
 }
